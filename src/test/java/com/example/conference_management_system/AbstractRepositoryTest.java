b3d38e5cb48623ae8fae38fed10cf3f142d85c00
ThanosL <thanos@example.com>
2024-03-20 14:07:08 +0200

    Removed unnecessary redis container from DataJpaTest

--- src/test/java/com/example/conference_management_system/AbstractRepositoryTest.java
+++ src/test/java/com/example/conference_management_system/AbstractRepositoryTest.java
@@ -4,14 +4,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 import com.example.conference_management_system.config.JpaAuditingConfig;
 
 
 //We have to import the JpaAuditingConfig class for the database to auto-generated the created date.
-
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({
@@ -26,12 +24,7 @@ public class AbstractRepositoryTest {
             .withPassword("test")
             .withDatabaseName("conference_ms_test");
 
-    @ServiceConnection
-    protected static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:alpine")
-            .withExposedPorts(6379);
-
     static {
         postgreSQLContainer.start();
-        redisContainer.start();
     }
 }

7442b4862d29e9bd63386692e14cc820441dd873
ThanosL <thanos@example.com>
2024-03-20 13:55:41 +0200

    Added documentation for the CustomTestSecurityContext

--- src/test/java/com/example/conference_management_system/security/CustomSecurityContextFactory.java
+++ src/test/java/com/example/conference_management_system/security/CustomSecurityContextFactory.java
@@ -12,25 +12,45 @@ import com.example.conference_management_system.role.RoleType;
 
 import java.util.Set;
 
+/*
+    Since we are creating a class that implements the UserDetails, we need to provide our own annotation for the mock
+    user, because the default one provider by Spring Security is not enough in this case, we might have extra properties
+    in our class. Creating that annotation will allow us to covert that to a custom mock spring security context that
+    will use the custom authentication object.
+
+    If we don't implement that, in a scenario where we test the below endpoint
+        ResponseEntity<Void> createConference(@Valid @RequestBody ConferenceCreateRequest conferenceCreateRequest,
+                                          @AuthenticationPrincipal SecurityUser securityUser,
+                                          UriComponentsBuilder uriBuilder,
+                                          HttpServletRequest servletRequest)
+    using the default @WithMockUser the SecurityUser securityUser will be null, because the principal of the
+    default authentication object provided by Spring is of type UserDetails and our tests will fail.
+
+    https://www.youtube.com/watch?v=onD_fyhy58o&list=PLEocw3gLFc8X_a8hGWGaBnSkPFJmbb8QP&index=39
+ */
 public class CustomSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
 
+    /*
+        This is the logic behind our custom annotation. We use the default values of our annotation and for the role we
+        pass the value specified @WithMockCustomUser(roles = "ROLE_PC_CHAIR"). If we had more roles we have to adjust
+        the logic
+     */
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
-        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        SecurityContext testSecurityContext = SecurityContextHolder.createEmptyContext();
         User user = new User();
         user.setUsername(annotation.username());
         user.setPassword(annotation.password());
         user.setRoles(Set.of(new Role(RoleType.valueOf(annotation.roles()[0]))));
 
         SecurityUser principal = new SecurityUser(user);
-
         Authentication authentication = new UsernamePasswordAuthenticationToken(
                 principal,
                 null,
                 principal.getAuthorities()
         );
-        securityContext.setAuthentication(authentication);
+        testSecurityContext.setAuthentication(authentication);
 
-        return securityContext;
+        return testSecurityContext;
     }
 }
--- src/test/java/com/example/conference_management_system/security/WithMockCustomUser.java
+++ src/test/java/com/example/conference_management_system/security/WithMockCustomUser.java
@@ -5,6 +5,15 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.springframework.security.test.context.support.WithSecurityContext;
 
+/*
+    We need to set the @Retention to RUNTIME, because by default it is SOURCE and our annotation will not be visible
+    to be intercepted via reflection at apps execution
+
+    https://www.youtube.com/watch?v=onD_fyhy58o&list=PLEocw3gLFc8X_a8hGWGaBnSkPFJmbb8QP&index=39
+
+    @WithSecurityContext(factory = CustomSecurityContextFactory.class) Whenever a test sees our custom annotation we
+    specify which security context to be used.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = CustomSecurityContextFactory.class)
 public @interface WithMockCustomUser {
